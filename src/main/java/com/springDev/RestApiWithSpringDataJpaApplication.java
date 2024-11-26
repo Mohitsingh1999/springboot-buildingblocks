85bcd181b26cb8504454aa1bd04f3eb6703f5bec
mohit <ms0135048@gmail.com>
2024-11-26 23:21:07 +0530

    first-commit

---
+++ src/main/java/com/springDev/RestApiWithSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.springDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiWithSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springDev/RestApiWithSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
